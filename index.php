9d5ace137ea60a1e30c6b16e40117844014a95f1
SanketG-G <gangardesanket790@gmail.com>
2026-05-27 15:48:58 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 
-<?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
+
 
 
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 /**
  * ---------------------------
